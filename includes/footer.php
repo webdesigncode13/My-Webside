28a3ab8a8eb90b2c25cf513866ed5c24c9bd025a
webdesigncode13 <163662307+webdesigncode13@users.noreply.github.com>
2024-03-16 21:29:23 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<!-- Footer -->
+	<footer class="bg-secondary" style="padding-bottom: 70px;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h5 class="text-info pt-5 mb-3">WEB DESIGN CODE</h5>
+					<p><strong>Web Design Code :</strong> It is a blog dedicated to providing valuable content about learning HTML, CSS, Javascript, Animated Personal Portfolio Website Design, Responsive Landing Page Design, Logo Design using HTML and CSS, Hover Effects, Creative CSS Animation, HTML5, CSS3, Javascript, and much more.</p>
+					<a href="about.php" class="btn btn-danger">Read More</a>
+				</div>
+				<div class="col-md-4">
+					<h5 class="text-info pt-5 mb-3">LINKS</h5>
+					<a class="nav-link text-dark mb-3" href="index.php"><strong>Home</strong></a>
+					<a class="nav-link text-dark mb-3" href="project.php"><strong>HTML CSS & JAVASCRIPT</strong></a>
+					<a class="nav-link text-dark mb-3" href="about.php"><strong>About Us</strong></a>
+				</div>
+				<div class="col-md-4">
+					<h5 class="text-info pt-5 mb-3">FOLLOW US</h5>
+					<a class="text-dark p-2" href="https://youtube.com/@WebDesign-013?si=OhQlVpqQFeZLn2iX" target="_blank"><strong><ion-icon class="pe-2" style="font-size: .8cm" name="logo-youtube"></ion-icon></strong></a>
+					<a class="text-dark p-2" href="https://t.me/webdesigncode" target="_blank"><strong><ion-icon class="pe-2" style="font-size: .8cm" name="paper-plane"></ion-icon></strong></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+<!-- End Footer -->
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+	<!-- Header -->
+		<header class="fixed-top" style="position: sticky; top: 0;">
+			<nav id="navbar" class="navbar navbar-expand-lg navbar-dark" style="background-color: #575757;">
+				<div class="container">
+				
+					<a href="index.php" class="navbar-brand">
+						<img loading="lazy" style="width: 80px; border-radius: 50%;" src="images/logo.jpg" alt="logo" loading="lazy">
+					</a>
+					<a href="index.php" class="nav-link">
+						<h2 class="text-info">Web Design Code</h2>
+					</a>
+          
+
+					<button class="navbar-toggler" style="width: 50px;" type="button" data-bs-toggle="collapse" data-bs-target="#navBar">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navBar">
+						<ul class="navbar-nav ms-auto" style="align-items: center;">
+							<li class="nav-item">
+								<a href="index.php" class="nav-link me-2 ">HOME</a>
+							</li>
+							<li class="nav-item">
+								<a href="project.php" class="nav-link me-2">HTML CSS & JAVASCRIPT</a>
+							</li>
+							<li class="nav-item">
+								<a href="about.php" class="nav-link me-2">ABOUT US</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+    <!-- End Header -->
